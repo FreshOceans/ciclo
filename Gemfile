affd63d74744ed1c603bf706416bc3720b7a6144
FreshOceans <mperezz398@gmail.com>
2017-09-21 09:33:20 -0400

    commit before branch switch

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'gon'
 gem 'nokogiri'
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'popper_js', '~> 1.11.1'
+gem 'burgermenu-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,30 +1,36 @@
-// $(document).on('turbolinks:load', function(){
-//
-//    //Navigation Menu Slider
-//     $('#nav-expander').on('click',function(e){
-//   		e.preventDefault();
-//   		$('body').toggleClass('nav-expanded');
-//   	});
-//   	$('#nav-close').on('click',function(e){
-//   		e.preventDefault();
-//   		$('body').removeClass('nav-expanded');
-//   	});
-//
-//   	// Initialize navgoco with default options
-//     $(".main-menu").navgoco({
-//         caret: '<span class="caret"></span>',
-//         accordion: false,
-//         openClass: 'open',
-//         save: true,
-//         cookie: {
-//             name: 'navgoco',
-//             expires: false,
-//             path: '/'
-//         },
-//         slide: {
-//             duration: 300,
-//             easing: 'swing'
-//         }
-//     });
-//
-// }); // End jQuery
+$(document).on('turbolinks:load', function(){
+
+    function burgermenu() {
+        $('#menu-example').burgermenu();
+    };
+
+   // //Navigation Menu Slider
+   //  $('#nav-expander').on('click',function(e){
+   // 	e.preventDefault();
+   // 	$('body').toggleClass('nav-expanded');
+   // });
+   // $('#nav-close').on('click',function(e){
+   // 	e.preventDefault();
+   // 	$('body').removeClass('nav-expanded');
+   // });
+   //
+   // // Initialize navgoco with default options
+   //  $(".main-menu").navgoco({
+   //      caret: '<span class="caret"></span>',
+   //      accordion: false,
+   //      openClass: 'open',
+   //      save: true,
+   //      cookie: {
+   //          name: 'navgoco',
+   //          expires: false,
+   //          path: '/'
+   //      },
+   //      slide: {
+   //          duration: 300,
+   //          easing: 'swing'
+   //      }
+   //  });
+
+   burgermenu();
+
+}); // End jQuery
